321121d82dea9996c258707dc66e89f95a793224
ZhaochenCai <731744067@qq.com>
2019-12-21 15:40:54 +0800

    remove default recmmond food

--- pages/dishes/dishes.js
+++ pages/dishes/dishes.js
@@ -31,7 +31,7 @@ Page({
     dishesDetail: {
       name: '白切肉',
       photo: 'http://p2.qhmsg.com/t01561d3c1fc9958d06.jpg',
-      description: '白切肉，是上海乡土气十足的家常冷菜，逢年过节，煮一方猪肉，趁热拆骨，待冷却后切成厚片，佐酱油蘸食。传入酒楼饭店，在选料和制作工艺上得以改进提高，成为上海十六浦德兴馆的看家菜。',
+      description: '',
       CanteenId: '0',
       window: "淮扬餐厅",
       price: "20",
