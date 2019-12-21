49885850b51b966e3c0d7ead0cd1f1c6a7440808
Bastian210 <bastian210@163.com>
2019-12-21 14:29:01 +0800

    解决openId为undfined的问题

--- app.js
+++ app.js
@@ -33,47 +33,48 @@ App({
             } else {
               reject(response.data);
             }
+
+            // 获取用户信息
+            wx.getSetting({
+              success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                  // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                  wx.getUserInfo({
+                    success: res => {
+                      // 可以将 res 发送给后台解码出 unionId
+                      console.log(res.userInfo)
+                      that.globalData.userInfo = res.userInfo
+                      that.globalData.hasUserInfo = true
+
+                      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                      // 所以此处加入 callback 以防止这种情况
+                      if (that.userInfoReadyCallback) {
+                        that.userInfoReadyCallback(res)
+                      }
+                      let tempData = {
+                        'wxID': that.globalData.openId,
+                        'userName': that.globalData.userInfo.nickName,
+                        'avatarUrl': that.globalData.userInfo.avatarUrl
+                      }
+                      console.log('enter-----', that.globalData.openId)
+                      http.post('/user/addUser', tempData).then((response) => {
+                        let tempData2 = {
+                          'wxId': that.globalData.openId
+                        }
+                        http.post('/user/getUserInfo', tempData2).then((response) => {
+                          that.globalData.userId = response.object.id
+                        })
+                      })
+                    }
+                  })
+                }
+              }
+            })
           },
           fail(error) {
             reject(error.data);
           }
         })
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              console.log(res.userInfo)
-              this.globalData.userInfo = res.userInfo
-              this.globalData.hasUserInfo = true
-              console.log(res)
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-              let tempData = {
-                'wxID': this.globalData.openId,
-                'userName': this.globalData.userInfo.nickName,
-                'avatarUrl': this.globalData.userInfo.avatarUrl
-              }
-              http.post('/user/addUser', tempData).then((response) => {
-                let tempData2 = {
-                  'wxId': this.globalData.openId
-                }
-                http.post('/user/getUserInfo', tempData2).then((response) => {
-                  this.globalData.userId = response.object.id
-                })
-              })
-            }
-          })
-        }
       }
     })
     // 获取系统状态栏信息
